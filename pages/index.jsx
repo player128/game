ee52fc2460839c65b749435861be9fb43264c79a
Slava <slavamonster6@gmail.com>
2025-01-24 14:24:14 +0300

    add modal

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { GameTitle, GameInfo, GameField, useGameState } from "../components/game";
 import { Header } from "../components/header";
 import { GameSymbol } from "../components/game/game-symbol";
+import { UiModal } from "../components/uikit/ui-modal";
+import { UiButton } from "../components/uikit/ui-button";
 
 export default function HomePage() {
   const [playersCount] = useState(4);
@@ -31,7 +33,18 @@ export default function HomePage() {
             <GameSymbol symbol = {winnerSymbol}/>
           </div>
         )}
-
+        <UiModal width='md' isOpen={winnerSymbol} onClose={() => console.log('close')}>
+          <UiModal.Header>Игра завершена!</UiModal.Header>
+          <UiModal.Body  className="">
+            <div className="text-sm">
+              <span className="text-teal-600">Победитель: Paromovevg</span>
+            </div>
+          </UiModal.Body>
+          <UiModal.Footer>
+            <UiButton size='md' variant='outline'>Вернуться</UiButton>
+            <UiButton size='md' variant='primary' >Играть снова</UiButton>
+          </UiModal.Footer>
+        </UiModal>
         <GameField 
           className="mt-6" 
           cells={cells}
